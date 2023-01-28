b681e165341f1f8c7cf5ffc1b4139754aec8a139
Vikas_Singh <Vikas_Singh@VikasPC>
2023-01-28 13:15:33 +0530

    seven commit

--- src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
+++ src/main/java/com/driver/services/impl/ParkingLotServiceImpl.java
@@ -65,7 +65,7 @@ public class ParkingLotServiceImpl implements ParkingLotService {
                }
            }
            parkingLotRepository1.save(parkingLot);
-//           spotRepository1.save(spot);
+           spotRepository1.save(ans);
            return ans;
     }
 
